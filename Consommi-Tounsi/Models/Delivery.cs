4b38ba3a167f3ad6bc80ab2717f2df2a7a8b576f
zeinebchihaoui <zeineb.chihaoui@esprit.tn>
2021-04-17 03:09:27 +0100

    2nd

--- Consommi-Tounsi/Models/Delivery.cs
+++ Consommi-Tounsi/Models/Delivery.cs
@@ -7,5 +7,18 @@ namespace Consommi_Tounsi.Models
 {
     public class Delivery
     {
+        public int id_deliv { get; set; }
+        public DateTime date_debut { get; set; }
+        public DateTime date_fin { get; set; }
+        public float fresh { get; set; }
+        public String location { get; set; }
+        public String moyenT { get; set; }
+        public float weight { get; set; }
+       /* public Administrator administrator { get; set; }
+        public int? Delivery_ManFK { get; set; }
+        public Delivery_Man Delivery_Man { get; set; }
+        public int? OrderFK { get; set; }
+        public Order order { get; set; }
+       */
     }
 }
