b704df0df884244af08cc6fe2c8580a5f092ae16
Manel Tabessi <manel.tabessi@esprit.tn>
2021-04-25 17:23:53 +0200

    manell11

---
+++ Consommi-Tounsi/Controllers/FacturesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Consommi_Tounsi.Controllers
+{
+    public class FacturesController : Controller
+    {
+        // GET: Factures
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
