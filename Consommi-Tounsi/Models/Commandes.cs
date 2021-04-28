3c295ad9b4cd932ad4d827f2cf9e5136b22d7b7c
Manel Tabessi <manel.tabessi@esprit.tn>
2021-04-29 00:23:54 +0200

    vall

---
+++ Consommi-Tounsi/Models/Commandes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Consommi_Tounsi.Models
+{
+    public class Commandes
+    {
+        public int id { get; set; }
+        public DateTime date_commande { get; set; }
+        public double prixtotale { get; set; }
+        public Payment_TYPE paymentType { get; set; }
+        public Product Product { get; set; }
+
+        public Client Client { get; set; }
+
+        //public Enume id { get; set; }
+        //public int id { get; set; }
+
+
+    }
+}
---
+++ Consommi-Tounsi/Models/Factures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Consommi_Tounsi.Models
+{
+    public class Factures
+    {
+        public int id { get; set; }
+        public DateTime date_de_depart { get; set; }
+        public Commandes Commandes { get; set; }
+
+
+    }
+}
